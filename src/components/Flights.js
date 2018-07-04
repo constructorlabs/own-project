b0e4646f22f25217b66e021dac5e8cbf30fa266f
Rafal <r.porzucek@gmail.com>
2018-07-04 14:12:02 +0100

    Added validation to form

--- src/components/Flights.js
+++ src/components/Flights.js
@@ -11,57 +11,41 @@ function Flights({ flights, currency }) {
           console.log(result.fare.total_price);
           return (
             <div key={index}>
-              <p>Holidays</p>
+              <h2>Holidays</h2>
               <li>Duration:{result.itineraries[0].outbound.duration} hrs</li>
               <p>
                 Departs at:{
                   result.itineraries[0].outbound.flights[0].departs_at
                 }
-              </p>
-              <p>
                 Arrives at:{
                   result.itineraries[0].outbound.flights[0].arrives_at
                 }
-              </p>
-              <p>
                 Origin Airport:{
                   result.itineraries[0].outbound.flights[0].origin.airport
                 }
-              </p>
-              <p>
                 Destination Airport:{
                   result.itineraries[0].outbound.flights[0].destination.airport
                 }
-              </p>
-              <p>
                 Airline:{
                   result.itineraries[0].outbound.flights[0].operating_airline
                 }
               </p>
-              <p>Coming back home</p>
+              <h2>Coming back home</h2>
               <li>Duration:{result.itineraries[0].inbound.duration}</li>
               <p>
                 Departs at:{result.itineraries[0].inbound.flights[0].departs_at}
-              </p>
-              <p>
                 Arrives at:{result.itineraries[0].inbound.flights[0].arrives_at}
-              </p>
-              <p>
                 Origin Airport:{
                   result.itineraries[0].inbound.flights[0].origin.airport
                 }
-              </p>
-              <p>
                 Destination Airport:{
                   result.itineraries[0].inbound.flights[0].destination.airport
                 }
-              </p>
-              <p>
                 Airline:{
                   result.itineraries[0].inbound.flights[0].operating_airline
                 }
               </p>
-              <h2>Total fare:{result.fare.total_price}</h2>
+              <h3>Total fare:{result.fare.total_price}</h3>
             </div>
           );
         })}
--- src/components/Search.js
+++ src/components/Search.js
@@ -12,44 +12,252 @@ class Search extends React.Component {
       adults: "1",
       children: "0",
       max_price: "1000",
-      currency: "USD"
+      currency: "USD",
+      errors: {
+        origin: "",
+        destination: "",
+        deraprture_date: "",
+        return_date: "",
+        adults: "",
+        children: "",
+        max_price: "",
+        currency: ""
+      }
     };
 
     this.handleChange = this.handleChange.bind(this);
-    this.handelSubmit = this.handelSubmit.bind(this);
-    // this.sendFlightData = this.sendFlightData.bind(this);
-  }
 
-  // sendFlightData(flightsData) {
-  //   this.props.receiver(flightsData);
-  // }
+    this.handelSubmit = this.handelSubmit.bind(this);
+  }
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
   }
 
+  hasThreeCharacters(string) {
+    return string.length === 3;
+  }
+
+  hasTenCharacters(string) {
+    return string.length === 10;
+  }
+
+  // isNumber() {
+  //   let { max_price, children, adults } = this.state;
+  //   let x = this.state;
+
+  //   if (isNaN(x)) {
+  //     alert("Must input numbers only");
+  //     return false;
+  //   }
+  // }
+
+  isNumber(input) {
+    return !isNaN(input) && input.length;
+  }
+
+  // isNumber(input) {
+  //   if (typeof input === "number") {
+  //     return input;
+  //   }
+  // }
+
+  validate() {
+    const { origin, destination, currency } = this.state;
+
+    let formValid = true;
+
+    if (!this.hasThreeCharacters(origin)) {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            origin: "Please use 3 character IATA code for your origin airport"
+          })
+        };
+      });
+      formValid = false;
+    } else {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            origin: ""
+          })
+        };
+      });
+    }
+
+    if (!this.hasThreeCharacters(destination)) {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            destination:
+              "Please use 3 character IATA code for your destination airport"
+          })
+        };
+      });
+
+      formValid = false;
+    } else {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            destination: ""
+          })
+        };
+      });
+    }
+
+    if (!this.hasThreeCharacters(currency)) {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            currency:
+              "Please enter a valid currency using following format e.g. USD"
+          })
+        };
+      });
+      formValid = false;
+    } else {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            currency: ""
+          })
+        };
+      });
+    }
+
+    const { departure_date, return_date } = this.state;
+
+    if (!this.hasTenCharacters(departure_date)) {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            departure_date: "Please use following format for dates: 2018-08-01"
+          })
+        };
+      });
+
+      formValid = false;
+    } else {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            departure_date: ""
+          })
+        };
+      });
+    }
+
+    if (!this.hasTenCharacters(return_date)) {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            return_date: "Please use following format for dates: 2018-08-01"
+          })
+        };
+      });
+
+      formValid = false;
+    } else {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            return_date: ""
+          })
+        };
+      });
+    }
+
+    const { adults, children, max_price } = this.state;
+
+    if (!this.isNumber(adults)) {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            adults: "Please provide a valid number of adult passangers"
+          })
+        };
+      });
+
+      formValid = false;
+    } else {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            adults: ""
+          })
+        };
+      });
+    }
+
+    if (!this.isNumber(children)) {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            children: "Please provide a valid number of children"
+          })
+        };
+      });
+
+      formValid = false;
+    } else {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            children: ""
+          })
+        };
+      });
+    }
+
+    if (!this.isNumber(max_price)) {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            max_price: "Please provide a valid number for maxiumum price"
+          })
+        };
+      });
+
+      formValid = false;
+    } else {
+      this.setState(function(prevState) {
+        return {
+          errors: Object.assign({}, prevState.errors, {
+            max_price: ""
+          })
+        };
+      });
+    }
+
+    return formValid;
+  }
+
   handelSubmit(event) {
     event.preventDefault();
-    fetch(
-      `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=yCrg1AtzcO3WOHM31Sq1Qd3iuOsiCSbR&origin=${
-        this.state.origin
-      }&destination=${this.state.destination}&departure_date=${
-        this.state.departure_date
-      }&return_date=${this.state.return_date}&adults=${
-        this.state.adults
-      }&max_price=${this.state.max_price}&currency=${this.state.currency}`
-    )
-      .then(function(response) {
-        return response.json();
-      })
-      .then(data => {
-        this.props.receiver(data);
-        // this.setState({
-        //   flightsData: data
-        // });
-        // this.sendFlightData(data);
-      })
-      .catch(err => console.log(err));
+    if (this.validate()) {
+      fetch(
+        `https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=yCrg1AtzcO3WOHM31Sq1Qd3iuOsiCSbR&origin=${
+          this.state.origin
+        }&destination=${this.state.destination}&departure_date=${
+          this.state.departure_date
+        }&return_date=${this.state.return_date}&adults=${
+          this.state.adults
+        }&max_price=${this.state.max_price}&currency=${this.state.currency}`
+      )
+        .then(function(response) {
+          return response.json();
+        })
+        .then(data => {
+          if (data.results) {
+            this.props.receiver(data);
+          } else {
+            alert("no results found");
+          }
+        })
+        .catch(err => console.log(err));
+    }
   }
 
   render() {
@@ -58,62 +266,102 @@ class Search extends React.Component {
         <h2>Search</h2>
         <p>Please fill in all the boxes using format provided and hit search</p>
         <form className="search" onSubmit={this.handelSubmit}>
-          <input
-            type="text"
-            name="origin"
-            placeholder="LON"
-            value={this.state.origin}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="destination"
-            placeholder="BOS"
-            value={this.state.destination}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="departure_date"
-            placeholder="2018-07-05"
-            value={this.state.departure_date}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="return_date"
-            placeholder="2018-07-10"
-            value={this.state.return_date}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="adults"
-            placeholder="Number of adults"
-            value={this.state.adults}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="children"
-            placeholder="Number of children"
-            value={this.state.children}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="max_price"
-            placeholder="Max Price"
-            value={this.state.max_price}
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="currency"
-            placeholder="USD"
-            value={this.state.currency}
-            onChange={this.handleChange}
-          />
+          <div>
+            <input
+              type="text"
+              name="origin"
+              placeholder="LON"
+              value={this.state.origin}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.origin ? (
+              <p>{this.state.errors.origin}</p>
+            ) : null}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="destination"
+              placeholder="BOS"
+              value={this.state.destination}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.destination ? (
+              <p>{this.state.errors.destination}</p>
+            ) : null}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="departure_date"
+              placeholder="2018-07-05"
+              value={this.state.departure_date}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.departure_date ? (
+              <p>{this.state.errors.departure_date}</p>
+            ) : null}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="return_date"
+              placeholder="2018-07-10"
+              value={this.state.return_date}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.return_date ? (
+              <p>{this.state.errors.return_date}</p>
+            ) : null}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="adults"
+              placeholder="Number of adults"
+              value={this.state.adults}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.adults ? (
+              <p>{this.state.errors.adults}</p>
+            ) : null}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="children"
+              placeholder="Number of children"
+              value={this.state.children}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.children ? (
+              <p>{this.state.errors.children}</p>
+            ) : null}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="max_price"
+              placeholder="Max Price"
+              value={this.state.max_price}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.max_price ? (
+              <p>{this.state.errors.max_price}</p>
+            ) : null}
+          </div>
+          <div>
+            <input
+              type="text"
+              name="currency"
+              placeholder="USD"
+              value={this.state.currency}
+              onChange={this.handleChange}
+            />
+            {this.state.errors.currency ? (
+              <p>{this.state.errors.currency}</p>
+            ) : null}
+          </div>
           <button type="submit">Search</button>
         </form>
       </div>
